56ba5e99cd06a8ad3fcc04ea7d3daab9431f4a65
Barnaby Colby <barnaby.colby@gmail.com>
2018-02-07 23:32:06 +0000

    Run tests when make.sh is called.

--- make.sh
+++ make.sh
@@ -3,6 +3,7 @@
 # Exit immediately if something goes wrong
 set -e
 
+# Build the project and tests
 BUILD=build
 mkdir -p ${BUILD}
 current_dir=$(pwd)
@@ -10,3 +11,6 @@ pushd ${BUILD}
 cmake ${current_dir}
 popd
 cmake --build ${BUILD}
+
+# Run the tests
+./${BUILD}/tests/run_tests
